cc39ba5ecbae5dc1d670421981cc3052e00f81e3
Franklin <fr@nkl.in>
2024-05-21 00:07:23 +0200

    Style: Added 'no default' comment on switch

--- index.js
+++ index.js
@@ -386,6 +386,7 @@ function output (level, what, result, describe) {
     case 'good': str = colorStr ('green', 'good'); break;
     case 'fail': str = (!isGithubAction ? colorStr ('red', 'FAIL') : ''); break;
     case 'warn': str = (!isGithubAction ? colorStr ('yellow', 'warn') : ''); break;
+    // no default
   }
 
   /* istanbul ignore next */
@@ -951,6 +952,7 @@ unitTests.isCondition = (level, what, one, operator, two) => {
     case '>': result.state = one > two; break;
     case '<=': result.state = one <= two; break;
     case '>=': result.state = one >= two; break;
+    // no default
   }
 
   output (level, what, result, describe);
