8ead7ac789f0ba598ed3d383e2c9784dde228b5e
Franklin <fr@nkl.in>
2026-01-04 23:22:58 +0100

    Test: Added .object checks

--- test.js
+++ test.js
@@ -101,6 +101,8 @@ doTest.add( 'Methods', ( test, fake ) => {
 
   doTest.test()
     .info( '.length: ' + doTest.length )
+    .object( { example: 'data' } )
+    .object( [ 'world', 123 ] )
     .isError( 'fail', 'test() .isError', new Error() )
     .isInstanceOf( 'fail', 'test() .isInstanceOf', TestClass, 'TestClass' )
     .isClass( 'fail', 'test() .isClass', TestClass )
